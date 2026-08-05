ced1048812e5b57962e952e7a52d6418ee29702c
JISHNU YASWANTH <jishnuy4@gmail.com>
2026-08-05 12:26:09 +0530

    Delete EEPROM directory

--- EEPROM/eeprom.h
+++
@@ -1,5 +0,0 @@
-
-void i2c_eeprom_bytewrite(u8 slaveAddr,u16 wBufAddr,u8 dat);
-void EEPROM_write(u8 slaveAddr,u16 wBufStartAddr,u8 *p,u8 nBytes);
-void EEPROM_Read(u8 slaveAddr,u16 rBufStartAddr,u8 *p,u8 nBytes);
-u8 i2c_eeprom_randomread(u8 slaveAddr,u8 rBufAddr);
