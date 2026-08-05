5dea7a1e8178497cc9b920e002e148880d0e7600
JISHNU YASWANTH <jishnuy4@gmail.com>
2026-08-05 09:54:04 +0530

    Add header file for SIPO 74HC164 functions

---
+++ SIPO Registers/sipo.h
@@ -0,0 +1,11 @@
+#ifndef SIPO_74HC164_H
+#define SIPO_74HC164_H
+
+#include "types.h"
+void Init_SIPO_74HC164(void);
+void SIPO_74HC164_1(u8);
+void SIPO_74HC164_2(u8);
+void SIPO_74HC164_3(u8);
+void SIPO_74HC164_4(u8);
+
+#endif
